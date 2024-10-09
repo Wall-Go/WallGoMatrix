7ef6156aa722f821e3a9694800d55b272ebdb87a
Philipp Schicho <schicho@icloud.com>
2024-10-09 17:27:45 +0200

    include pacletInfo

---
+++ PacletInfo.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+(* created 2024/10/09*)
+
+Paclet[
+    Name -> "WallGoMatrix",
+    Version -> "0.1.0",
+    MathematicaVersion -> "13+",
+    Creator -> "Andreas Ekstedt, Oliver Gould, Joonas Hirvonen, Benoit Laurent, Lauri Niemi, Philipp Schicho, Jorinde van de Vis"
+]
