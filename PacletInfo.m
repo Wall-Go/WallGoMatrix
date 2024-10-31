5f0a6d004dc525d80ef6af916a8f0c97f9101fac
Philipp Schicho <schicho@icloud.com>
2024-10-31 16:01:29 +0100

    update paclet info

--- PacletInfo.m
+++ PacletInfo.m
@@ -6,13 +6,17 @@ PacletObject[
     "Description" -> "Computes 2-to-2 scattering matrix amplitudes for arbitrary quantum field theories.",
     "Creator" -> "Andreas Ekstedt, Oliver Gould, Joonas Hirvonen, Benoit Laurent, Lauri Niemi, Philipp Schicho, Jorinde van de Vis",
     "URL" -> "https://github.com/Wall-Go/WallGoMatrix",
+    "License" -> "GPL-3.0+",
     "PublisherID" -> "WallGo",
-    "Version" -> "0.2.0",
+    "Version" -> "0.2.1",
     "Extensions" -> {
       {
         "Kernel",
         "Root" -> "Kernel",
-        "Context" -> "WallGoMatrix`"
+        "Context" -> {
+			"WallGoMatrix`",
+			"WallGoMatrix.m"
+		}
       }
     },
     "MathematicaVersion" -> "13+"
