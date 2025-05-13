231722a31c8a30d0fd3640e2ad010971e4dd1e54
Philipp Schicho <schicho@icloud.com>
2025-05-13 15:09:33 +0200

    add feynRule generating file

---
+++ tests/FeynCalc/sunHiggsYukawa/feynRules.m
@@ -0,0 +1,24 @@
+(* ::Package:: *)
+
+Quit[];
+
+
+(* ::Input:: *)
+(*$FeynRulesPath=DirectoryName[FindFile["FeynRules`"]];*)
+(*<<FeynRules`*)
+
+
+(* ::Input:: *)
+(*(* load model file *)*)
+(*model="sun-higgs-yukawa";*)
+(*modelFile =FileNameJoin[{ NotebookDirectory[],model<>".fr"}];*)
+(*LoadModel[modelFile]*)
+
+
+(* ::Input:: *)
+(*(* choosing output location *)*)
+(*outputFiles=FileNameJoin[NotebookDirectory[]<>model];*)
+(*(* writing FeynArts output *)*)
+(*WriteFeynArtsOutput[L,Output->outputFiles,CouplingRename->False]*)
+(*(* deleting empty directory made by FeynRules *)*)
+(*DeleteDirectory[outputFiles]*)
