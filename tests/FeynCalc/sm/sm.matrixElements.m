84e24ca6bb25050f8cc42852b9e3e718c4cab07b
Philipp Schicho <schicho@icloud.com>
2025-08-15 18:10:11 +0200

    tidy sm matrix elements Feyncalc generation file

--- tests/FeynCalc/sm/sm.matrixElements.m
+++ tests/FeynCalc/sm/sm.matrixElements.m
@@ -37,39 +37,39 @@ InitializeModel["SMQCD"];
 
 
 particleTypes={
-F[3,{3}](* t-quark *),
--F[3,{3}](* anti-t-quark *),
-F[4,{3}](* b-quark *),
--F[4,{3}](* anti-b-quark *),
-F[2,{3}](* tauon *),
--F[2,{3}](* anti-tauon *),
-F[1,{3}](* tau-neutrino *),
--F[1,{3}](* anti-tau-neutrino *),
-(*F[3,{2}](* charm *),
--F[3,{2}](* anti-charm *),
-F[4,{2}](* strange *),
--F[4,{2}](* anti-strange *),
-F[2,{2}](* muon *),
--F[2,{2}](* anti-muon *),
-F[1,{2}](* mu-neutrino *),
--F[1,{2}](* anti-mu-neutrino *),
-F[3,{1}](* up *),
--F[3,{1}](* anti-up *),
-F[4,{1}](* down *),
--F[4,{1}](* anti-down *),
-F[2,{1}](* electron *),
--F[2,{1}](* anti-electron *),
-F[1,{1}](* electron-neutrino *),
--F[1,{1}](* anti-electron-neutrino *),*)
-V[5](* g *),
-V[3](* W- *),
--V[3](* W+ *),
-V[2](* Z *),
-V[1](* photon *),
-S[1](* physical Higgs *),
-S[2](* uncharged Goldstone *),
-S[3](* charged Goldstone*),
--S[3](* charged anti-Goldstone*)
+	F[3,{3}](* t-quark *),
+	-F[3,{3}](* anti-t-quark *),
+	F[4,{3}](* b-quark *),
+	-F[4,{3}](* anti-b-quark *),
+	F[2,{3}](* tauon *),
+	-F[2,{3}](* anti-tauon *),
+	F[1,{3}](* tau-neutrino *),
+	-F[1,{3}](* anti-tau-neutrino *),
+	(*F[3,{2}](* charm *),
+	-F[3,{2}](* anti-charm *),
+	F[4,{2}](* strange *),
+	-F[4,{2}](* anti-strange *),
+	F[2,{2}](* muon *),
+	-F[2,{2}](* anti-muon *),
+	F[1,{2}](* mu-neutrino *),
+	-F[1,{2}](* anti-mu-neutrino *),
+	F[3,{1}](* up *),
+	-F[3,{1}](* anti-up *),
+	F[4,{1}](* down *),
+	-F[4,{1}](* anti-down *),
+	F[2,{1}](* electron *),
+	-F[2,{1}](* anti-electron *),
+	F[1,{1}](* electron-neutrino *),
+	-F[1,{1}](* anti-electron-neutrino *),*)
+	V[5](* g *),
+	V[3](* W- *),
+	-V[3](* W+ *),
+	V[2](* Z *),
+	V[1](* photon *),
+	S[1](* physical Higgs *),
+	S[2](* uncharged Goldstone *),
+	S[3](* charged Goldstone*),
+	-S[3](* charged anti-Goldstone*)
 };
 
 particleNames=Map[ToString[TheLabel[#]]<>If[Head[#]===Times,"bar",""]&,particleTypes]
@@ -148,18 +148,18 @@ polsums[x_,vec_,aux_,spinfac_]:=
 
 
 insertCouplingsForMasses={
-SMP["m_e"]->0,
-SMP["m_mu"]->0,
-SMP["m_tau"]->0,
-SMP["m_u"]->0,
-SMP["m_d"]->0,
-SMP["m_c"]->0,
-SMP["m_s"]->0,
-SMP["m_t"]->yt vev,
-SMP["m_b"]->0,
-SMP["m_H"]->Sqrt[2 lam]vev,
-SMP["m_W"]->1/2 SMP["g_W"]vev,
-SMP["m_Z"]->1/2 Sqrt[SMP["g_W"]^2+SMP["g'_W"]^2]vev
+	SMP["m_e"]->0,
+	SMP["m_mu"]->0,
+	SMP["m_tau"]->0,
+	SMP["m_u"]->0,
+	SMP["m_d"]->0,
+	SMP["m_c"]->0,
+	SMP["m_s"]->0,
+	SMP["m_t"]->yt vev,
+	SMP["m_b"]->0,
+	SMP["m_H"]->Sqrt[2 lam]vev,
+	SMP["m_W"]->1/2 SMP["g_W"]vev,
+	SMP["m_Z"]->1/2 Sqrt[SMP["g_W"]^2+SMP["g'_W"]^2]vev
 };
 
 
